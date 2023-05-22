585cd41b0d3efde875ecc2642d1523cce690f887
Todd B. Smith <elliptic1@gmail.com>
2023-05-21 23:06:47 -0400

    Refactoring the prompts a bit

--- podcastgenerator.py
+++ podcastgenerator.py
@@ -31,8 +31,8 @@ job_post_title = get_job_title()
 interviewee_voice = get_interviewee_voice()
 interviewee_old_company = get_company()
 interviewee_old_job = get_job_title()
-num_questions = 3
-total_num_questions = 3
+num_questions_per_response = 1
+total_num_questions = 80
 interviewer_voice = "Arthur"
 interview_type = get_interview_type()
 
@@ -81,7 +81,7 @@ human_template = """
 
 {format_instructions}
 
-YOUR VALID JSON CODE RESPONSE:
+YOUR RFC8259 compliant JSON RESPONSE:
 """
 
 human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
@@ -109,18 +109,20 @@ while not got_intro:
         intro_outro_response_json = output_parser.parse(output.content)
         got_intro = True
     except OutputParserException:
+        print(output.content)
+        print("Failed to parse intro and outro. Trying again...")
         intro_outro_response_json = {}
 
 response_schemas = [
     ResponseSchema(name="Question1", description="This is question 1"),
     ResponseSchema(name="Answer1", description="This is answer 1"),
     ResponseSchema(name="Response1", description="This is response 1"),
-    ResponseSchema(name="Question2", description="This is question 2"),
-    ResponseSchema(name="Answer2", description="This is answer 2"),
-    ResponseSchema(name="Response2", description="This is response 2"),
-    ResponseSchema(name="Question3", description="This is question 3"),
-    ResponseSchema(name="Answer3", description="This is answer 3"),
-    ResponseSchema(name="Response3", description="This is response 3"),
+    # ResponseSchema(name="Question2", description="This is question 2"),
+    # ResponseSchema(name="Answer2", description="This is answer 2"),
+    # ResponseSchema(name="Response2", description="This is response 2"),
+    # ResponseSchema(name="Question3", description="This is question 3"),
+    # ResponseSchema(name="Answer3", description="This is answer 3"),
+    # ResponseSchema(name="Response3", description="This is response 3"),
 ]
 
 # How you would like to parse your output
@@ -132,7 +134,6 @@ Ask {interview_type} interview questions related to the job of {job_post_title} 
 from the point of view of a {interviewer_title} from {company}.
 For each question, preface it by an introduction of the question so {interviewee_voice} has the full context.
 The question should be written in a normal speaking style and sound like a real person.
-All output should be valid parsable JSON.
 
 Answer Instructions:
 Generate a thorough and detailed answer from {interviewee_voice} for each question using the STAR method.
@@ -141,19 +142,18 @@ The STAR method is a technique you can use to answer interview questions.
 STAR stands for situation, task, action and result.
 Do not mention the STAR method or "situation", "task", "action", "result" in your answer.
 The answer should be written in a normal speaking style and sound like a real person.
-All output should be valid parsable JSON.
 
 Answer Response Instructions:
 For each answer, generate a thoughtful response from {interviewer_voice}.
 The response should not contain a question.
 The response should be one sentence long and should be a statement, not a question.
 The response should be written in a normal speaking style and sound like a real person.
-All output should be valid parsable JSON.
 
 General Instructions:
 There should be no markdown, no formatting, no newlines, no quotation marks, and no tabs in your output.
-Generate {num_questions} questions and answers and responses.
-All output should be valid parsable JSON.
+Generate {num_questions_per_response} questions and answers and responses.
+Do not include any explanations, only provide a RFC8259 compliant JSON response.
+Do not prefix the output with 'json' or anything else.
 """
 
 system_message_prompt = SystemMessagePromptTemplate.from_template(system_template_shared + system_template)
@@ -162,7 +162,7 @@ human_template = """
 
 {format_instructions}
 
-YOUR VALID JSON CODE RESPONSE:
+YOUR RFC8259 compliant JSON RESPONSE:
 """
 
 human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
@@ -174,7 +174,7 @@ _input = chat_prompt.format_prompt(
     interviewer_title=interviewer_title,
     company=company,
     job_post_title=job_post_title,
-    num_questions=num_questions,
+    num_questions=num_questions_per_response,
 )
 
 filenames = []
@@ -197,11 +197,13 @@ while (len(filenames) - 5) / 3 < total_num_questions:
             response_json = output_parser.parse(output.content)
             got_json = True
         except OutputParserException:
+            print(output.content)
+            print("Failed to parse json. Trying again...")
             response_json = {}
 
     # Create the audio files
     print(f"creating audio files for batch {batch}")
-    filenames += create_qa_audio_files(response_json, interviewee_voice, num_questions,
+    filenames += create_qa_audio_files(response_json, interviewee_voice, num_questions_per_response,
                                        interviewer_voice, batch)
 
 filenames += create_outro_audio_files(intro_outro_response_json, interviewee_voice, interviewer_voice)
